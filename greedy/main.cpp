4307d0fcb211c4090c6e691dec6999361e37e580
iftekharx <iftekharx86@gmail.com>
2023-05-10 17:48:44 +0600

    first commit

---
+++ greedy/main.cpp
@@ -0,0 +1,21 @@
+// what is greedy algorithm
+/* In greedy algorithm a set of resources are recursively divided based
+on the maximum, immediate availability of that resoruce at any given 
+stage of execution */
+
+// two stages:
+/*1. Scanning the list of items
+  2. Optimization
+  */
+
+ // requirements
+ // 1. reqcursion
+ // 2. context switching
+
+ // Conditions
+ /* 1. Each stepwise solution must structure a problem towards
+ its best accepted solution.
+ 2. It is sufficient if the structuring of the problem can halt
+ in a finite number of greedy steps.*/
+
+ 
