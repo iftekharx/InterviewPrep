8e380c55a9f768f2e1dc6664b961e3cc5b16a6a3
iftekharx <iftekharx86@gmail.com>
2023-05-10 18:19:16 +0600

    architecture added

--- greedy/main.cpp
+++ greedy/main.cpp
@@ -46,4 +46,34 @@ by scanning a list of items only once and considering certain activities.
 //   2              6                           9
 //   3              8                           9
 //   4              4                           8
-/* 1. */
+/* 1. In the activity scheduling example there is a 'start' and a 'finish time' for
+every activity. Each activity is indexed by a number for reference. There are two 
+activity categories.
+  a. considered activity: is the Activity, which is the reference from which the 
+  abilityto do more than one remaining activity is analyzed.
+  b. remaining activity: activities at one or more indexes ahead of considered 
+  activity.
+*/
+
+// the total duration gives the cost of performing the activity. That is 
+/*(finish - start) gives us the durational as the cost of an activity.
+ // You will learn that the greedy extent is the number of remaining activities 
+ you can perform in the time of a considered activity.
+*/
+
+// Architecture of the Greedy Approach
+
+/* step1: Scan the list of activity costs, starting with index 0 as the considered
+index.*/
+
+/*step2: When more activities can be finished by the time, the considered activity
+finishes, start searching for one or more remaining activities.*/
+
+/*step3: If there are no more remaining activities, the current remaining activity 
+becomes the next considered activity. Repeat step 1 and step 2, with the new 
+considered activity. If there are no remaining activities left, go to step 4.*/
+
+/*step4: Return the union of considered indices. These are the activity indices 
+that will be used to maximize throughput.*/
+
+
