e42d335b4b5c8973fdc2e77e136846a062b90eb1
iftekharx <iftekharx86@gmail.com>
2023-05-10 18:39:45 +0600

    intro

---
+++ greedy/main.py
@@ -0,0 +1,15 @@
+# Objective: Maximize Gold
+
+# Rules
+"""
+1. Can move in any direction: up, down, left, right
+2. Once a path is taken, there is no going back
+3. Game ends when no more steps can be taken.
+"""
+
+"""
+        1
+    1 {you}  3
+        2
+"""
+
