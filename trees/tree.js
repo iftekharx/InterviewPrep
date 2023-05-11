7d32496a2167ec7f91ff1ebdb3e9db8c7b53dd3e
iftekharx <iftekharx86@gmail.com>
2023-05-11 11:41:36 +0600

    tree search added

--- trees/tree.js
+++ trees/tree.js
@@ -91,3 +91,28 @@ const breadthFirstValues = (root) => {
 }
 
 console.log(breadthFirstValues(a))
+
+const treeIncludes = (root, target) => {
+  if (root === null) return false
+
+  const queue = [root]
+  while (queue.length > 0) {
+    const current = queue.shift()
+    if (current.val === target) return true
+
+    if (current.left) queue.push(current.left)
+    if (current.right) queue.push(current.right)
+  }
+  return false
+}
+
+const treeIncludesR = (root, target) => {
+  // recursive
+  if (root === null) return false
+
+  if (root.val === target) return true
+
+  return treeIncludes(root.left, target) || treeIncludes(root.right, target)
+}
+
+console.log(treeIncludesR(a, 'b'))
