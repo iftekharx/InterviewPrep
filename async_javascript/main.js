356d582c89e036e3433dc8b6c246074cd32dabc0
iftekharx <iftekharx86@gmail.com>
2023-05-13 19:21:44 +0600

    recursive setTimeout added

--- async_javascript/main.js
+++ async_javascript/main.js
@@ -4,3 +4,18 @@
 // promises, async await, event loop
 
 // javascript is single threaded
+function greet() {
+  console.log('hello')
+}
+
+// const timer = setTimeout(greet, 2000)
+// clearTimeout(timer) // cancels timer
+
+// const intervalId = setInterval(greet, 2000) // every 2 seconds it runs
+// clearInterval(intervalId)
+
+// recursive setTimeout
+setTimeout(function run() {
+  console.log('hello')
+  setTimeout(run, 100)
+}, 100)
