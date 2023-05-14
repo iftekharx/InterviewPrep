37e15c5ea5747f572816b2632196143a492581aa
iftekharx <iftekharx86@gmail.com>
2023-05-14 09:54:07 +0600

    Promises done

--- async_javascript/main.js
+++ async_javascript/main.js
@@ -30,12 +30,44 @@ function greetBob(greetFn) {
 greetBob(greet)
 
 // Promises
+const onFullfillment = (result) => {
+  console.log(result)
+  console.log('set up the table to eat tacos')
+}
+
+const onRejection = (error) => {
+  console.log(error)
+  console.log('Start cooking pasta')
+}
 
 const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve()
-  }, 5000)
+  resolve()
 })
 
-promise.then() // pending to fulfilled
-promise.catch() // pending to rejected
+promise.then(onFullfillment).catch(onRejection) // pending to fulfilled
+
+const promise1 = Promise.resolve(3)
+const promise2 = 42
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo')
+})
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values)
+}) // if one promise promise.all will reject with that message
+
+//Promise.allSettled - Waits for all input promises to complete
+//regardless of whether or not one of them is rejected
+
+//Promise.race() - returns a promise that fulfills or rejects as soon as one of the input promises
+//fullfills or rejects, with the value or reason from that promise
+const promise4 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 500, 'one')
+})
+const promise5 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'two')
+})
+
+Promise.race([promise4, promise5]).then((value) => {
+  console.log(value)
+})
