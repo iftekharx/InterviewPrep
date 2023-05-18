7e597b11ae0d6efa6846e53ff674a6f49756dc5a
iftekharx <iftekharx86@gmail.com>
2023-05-18 10:17:10 +0600

    algorithms in java practice started

---
+++ algorithms_in_java/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+
+    public static int[] resize(int[] arr, int capacity){
+        int[] temp = new int[capacity];
+        for(int i = 0; i < arr.length; i++){
+            temp[i] = arr[i];
+        }
+        arr = temp; // garbage collection finally
+        return arr;
+    }
+    
+    public static boolean isPallindrome(String word) {
+    	char[] charArray = word.toCharArray();
+    	int start = 0;
+    	int end = word.length() - 1;
+    	
+    	while(start < end) {
+    		if(charArray[start] != charArray[end]) {
+    			return false;
+    		}
+    		start ++;
+    		end--;
+    	}
+    	return true;
+    }
+
+    public static void main(String[] args) {
+//        int[] arr = {1, 2, 3, 4, 5};
+//
+//        arr = resize(arr, 8);
+//        arr[5] = 78;
+//
+//        System.out.println(arr.length);
+    	
+    	String x = "madam";
+    	System.out.println(isPallindrome(x));
+        
+    }
+    
+}
