de36c2ba97abc7a627f97e9b5f050253142a75a5
Iftekhar Hyder <iftekharx86@gmail.com>
2023-08-14 11:09:14 +0600

    materia ui started

---
+++ material_ui/react-mui-demo/src/components/MuiButton.tsx
@@ -0,0 +1,127 @@
+import {
+  Button,
+  Stack,
+  IconButton,
+  ButtonGroup,
+  ToggleButton,
+  Typography,
+  ToggleButtonGroup,
+} from "@mui/material";
+import React, { useState } from "react";
+import SendIcon from "@mui/icons-material/Send";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
+
+export const MuiButton = () => {
+  const [formats, setFormats] = useState<string[]>([]);
+
+  const handleFormatChange = (
+    _event: React.MouseEvent<HTMLElement>,
+    updatedFormats: string[]
+  ) => {
+    setFormats(updatedFormats);
+  };
+
+  return (
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        <Button variant="text" href="https://google.com">
+          Text
+        </Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+      </Stack>
+
+      <Stack display={"block"} spacing={2} direction={"row"}>
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          Large
+        </Button>
+      </Stack>
+
+      <Stack spacing={2} direction={"row"}>
+        <Button variant="contained" startIcon={<SendIcon />} disableRipple>
+          Send
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />} disableElevation>
+          Send
+        </Button>
+        <IconButton aria-label="send" color="success" size="small">
+          <SendIcon />
+        </IconButton>
+      </Stack>
+
+      <Stack direction={"row"}>
+        <ButtonGroup variant="contained">
+          <Button>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
+      <Stack direction={"row"}>
+        <ButtonGroup
+          variant="outlined"
+          orientation="vertical"
+          size="small"
+          color="secondary"
+          aria-aria-label="alignment button group"
+        >
+          <Button>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
+
+      <Stack direction={"row"}>
+        <Typography variant="h3" color={"success"}>
+          {formats}
+        </Typography>
+      </Stack>
+
+      <Stack direction={"row"}>
+        <ToggleButtonGroup
+          aria-label="text-formatting"
+          value={formats}
+          onChange={handleFormatChange}
+        >
+          <ToggleButton value={"bold"} aria-label="bold">
+            <FormatBoldIcon />
+          </ToggleButton>
+          <ToggleButton value={"italic"} aria-label="italic">
+            <FormatItalicIcon />
+          </ToggleButton>
+          <ToggleButton value={"underline"} aria-label="underline">
+            <FormatUnderlinedIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </Stack>
+    </Stack>
+  );
+};
---
+++ material_ui/react-mui-demo/src/components/MuiTypography.tsx
@@ -0,0 +1,60 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+export const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">h1 Heading</Typography>
+      <Typography variant="h2">h2 Heading</Typography>
+      <Typography variant="h3">h3 Heading</Typography>
+      <Typography variant="h4" component="h1" gutterBottom>
+        h4 Heading
+      </Typography>
+      <Typography variant="h5">h5 Heading</Typography>
+      <Typography variant="h6">h6 Heading</Typography>
+
+      <Typography variant="subtitle1">Subtitle 1</Typography>
+      <Typography variant="subtitle2">Subtitle 2</Typography>
+
+      <Typography variant="body1">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam, quia
+        assumenda, reiciendis nihil id consectetur earum vero sapiente
+        recusandae tempore officia reprehenderit eligendi doloremque est et in
+        repudiandae nemo dignissimos. Non dolor error modi labore! Fuga soluta
+        alias illum rem quisquam distinctio error earum reiciendis corrupti
+        aliquam provident, minima modi corporis explicabo consequuntur quod
+        recusandae amet nostrum cum perspiciatis. Repellat. Facere, magni
+        assumenda commodi dicta saepe aliquid. Laudantium soluta eius ipsam a
+        dolor nobis omnis quod sed. Tempora, numquam! Repudiandae facilis
+        ratione eius omnis, voluptatum maxime officiis aspernatur quaerat iure.
+        Itaque illo quibusdam fugit, consequatur sit atque, soluta, quae
+        asperiores aliquid quam ducimus perspiciatis debitis architecto quod ad!
+        Nostrum porro non libero praesentium similique nesciunt suscipit
+        consequuntur quasi quam! Itaque. Adipisci, fuga dolore assumenda totam,
+        nobis beatae soluta ipsam unde possimus earum eos aspernatur numquam.
+        Similique repudiandae corrupti excepturi est esse tenetur odio! Corporis
+        quo, accusantium aut reprehenderit pariatur placeat.
+      </Typography>
+
+      <Typography variant="body2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+        explicabo reiciendis temporibus molestiae perferendis similique porro,
+        sunt quibusdam ratione magnam atque. Id recusandae et voluptates
+        veritatis fugiat magni esse provident? Doloribus reprehenderit beatae
+        vero tenetur saepe commodi excepturi deserunt repudiandae sunt, dolor,
+        vitae eius ad iure provident delectus illum perspiciatis cupiditate quod
+        cumque accusamus quidem voluptates ea ratione? Excepturi, nisi. Fugiat
+        ipsa nemo assumenda delectus quasi ducimus incidunt quos tenetur vel
+        sunt dolor temporibus eum cumque accusantium mollitia sit ut, molestias
+        illo, quisquam aspernatur officia repellat. Quia labore suscipit
+        laborum! Molestiae, praesentium omnis non ducimus, ipsa suscipit dolore
+        iste animi sit architecto illo? Fuga blanditiis, alias minus provident
+        tempore, ipsum in eveniet cupiditate officiis ad, mollitia
+        necessitatibus explicabo voluptatum nihil. Repellat, amet? Beatae porro
+        quisquam, harum minima dicta non. Sapiente quidem cumque officia nobis
+        eos numquam ipsam sunt iure repudiandae molestiae, nulla voluptatum
+        dolore possimus quis facilis itaque? Aliquam, at?
+      </Typography>
+    </div>
+  );
+};
